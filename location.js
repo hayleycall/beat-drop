b20bbdd870dd1f1f86e852c874ac00b9c5fb62c6
britne <britnesissom@gmail.com>
2015-10-10 01:01:05 -0500

    undo

--- location.js
+++ location.js
@@ -18,6 +18,17 @@ function initMap() {
       infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
       map.setCenter(pos);
+
+      var cityCircle = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: pos,
+      radius: 1609    // 1 mi. radius
+    });
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
